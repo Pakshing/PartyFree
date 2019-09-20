6b589345998817b4d78613a7bf4477967024bc90
Pakshing <55418556+Pakshing@users.noreply.github.com>
2019-09-20 16:24:26 -0700

    Update make_call.py

--- make_call.py
+++ make_call.py
@@ -1,9 +1,8 @@
 from twilio.rest import Client
 from twilio.twiml.voice_response import Play, VoiceResponse
-account_sid = 'AC94a0517d20fc9a6ad2eee9d5423791b7'
-auth_token = '55c574509ef373551518e269a64f03a7'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
-ToCall = "+19162774165"
 CallFrom = "+19167961080"
 
 print("This program will call you when you need it. And it will play a pre-recorded drunk voice message, so you can have perfect exercute to leave the meeting or party.")
